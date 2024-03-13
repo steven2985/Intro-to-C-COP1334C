4588c16d2ea029035aeaac6a5dd87cea41f03d62
Steven Fernandez <47422305+steven2985@users.noreply.github.com>
2024-03-13 14:42:01 -0400

    Add files via upload

---
+++ Homework 1/Class hello world/Class hello world/hello.cpp
@@ -0,0 +1,20 @@
+/*
+   Steven Fernandez
+
+   hello.cpp
+
+   Display the message "Hello World!" 
+*/
+
+// Header Files
+#include <iostream>
+using namespace std; 
+
+int main()
+{
+	// Display message 
+	cout << "Hello World!!!" << endl;
+
+	system("pause"); 
+	return 0;
+}
